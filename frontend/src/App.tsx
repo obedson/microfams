ac585b0abd259b6e112ef3f3492823238d135c55
peerflo <team.peerflo@gmail.com>
2026-08-24 15:33:14 +0000

    Add booking dispute resolution client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -65,6 +65,7 @@ import BudgetVsActual from './pages/BudgetVsActual';
 import CashFlow from './pages/CashFlow';
 import BalanceSheet from './pages/BalanceSheet';
 import CreditProducts from './pages/CreditProducts';
+import BookingDisputeResolution from "./pages/BookingDisputeResolution";
 import BookingDispute from "./pages/BookingDispute";
 import BookingRecovery from "./pages/BookingRecovery";
 import InvestmentRefundRecognition from "./pages/InvestmentRefundRecognition";
@@ -124,6 +125,7 @@ function App() {
         <Route path="/accounting/cash-flow" element={<ProtectedRoute><CashFlow /></ProtectedRoute>} />
         <Route path="/accounting/balance-sheet" element={<ProtectedRoute><BalanceSheet /></ProtectedRoute>} />
         <Route path="/credit/products" element={<ProtectedRoute><CreditProducts /></ProtectedRoute>} />
+        <Route path="/bookings/disputes/resolution" element={<ProtectedRoute><BookingDisputeResolution /></ProtectedRoute>} />
         <Route path="/bookings/disputes" element={<ProtectedRoute><BookingDispute /></ProtectedRoute>} />
         <Route path="/bookings/recovery" element={<ProtectedRoute><BookingRecovery /></ProtectedRoute>} />
         <Route path="/investments/refund-obligations" element={<ProtectedRoute><InvestmentRefundRecognition /></ProtectedRoute>} />
